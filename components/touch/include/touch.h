8861d2bedef0a87490679d4041d55087e69cccbc
novaeco <novatribolnovea@gmail.com>
2025-07-13 23:27:36 +0200

    Add touch controller driver with LVGL input

--- components/touch/include/touch.h
+++ components/touch/include/touch.h
@@ -8,3 +8,14 @@ esp_err_t touch_init(void);
 /** Read touch coordinates, returns true if touched */
 bool touch_read(uint16_t *x, uint16_t *y);
 
+typedef struct {
+    uint16_t x0;
+    uint16_t y0;
+    uint16_t x1;
+    uint16_t y1;
+} touch_calibration_t;
+
+esp_err_t touch_calibration_load(touch_calibration_t *cal);
+esp_err_t touch_calibration_save(const touch_calibration_t *cal);
+
+
