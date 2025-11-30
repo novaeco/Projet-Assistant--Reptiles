cd676da85d4c6e11b843491aa76dd248be8f410a
novaeco <novatribolnovea@gmail.com>
2025-11-30 15:04:59 +0100

    Delete components directory

--- components/buttons/include/buttons.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "esp_err.h"
-
-esp_err_t buttons_init(void);
--- components/menu/include/menu.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <stdint.h>
-void menu_update(void);
-void menu_process_keys(uint16_t keys);
